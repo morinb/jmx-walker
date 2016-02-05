f7eb2e4127793da320dfc1fe7497e7f96cb2c48b
Baptiste MORIN <morin.baptiste@gmail.com>
2016-02-05 12:55:31 +0100

    remove generated pom

---
+++ jmx-walker-launcher.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ ! -d jmx-walker ]; then
+    echo "Cloning jmx-walker"
+    git clone http://atpds4.fr.net.intra/bmo/jmx-walker.git jmx-walker
+fi
+
+cd jmx-walker
+echo "Getting last jmx-walker version"
+git pull origin master
+./sbt/bin/sbt.bat -mem 128 run
